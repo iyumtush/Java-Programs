7f6e8b22392eaf7f47c2bf883a9789e4c54f5131
iyumtush <tusharcse0@gmail.com>
2026-07-09 20:34:30 +0530

    Largest number find from array

---
+++ src/TushCodes/FindLargestNumArray.java
@@ -0,0 +1,27 @@
+package TushCodes;
+
+public class FindLargestNumArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	
+		
+		int num[] = {10,40,50,100,150,60,19,22,0,13};
+		
+		int largest = num[0];
+		
+		for(int i = 0 ; i<num.length;i++) {
+			
+			if(num[i]>largest) {
+				largest = num[i];
+				
+				
+			}
+			
+		}
+		System.out.println("The Largest Number is : "+largest);
+
+	}
+
+}
