cde98bcf1d1791e6d0571954a207237f5bebffc7
iyumtush <tusharcse0@gmail.com>
2026-07-11 21:21:31 +0530

    String UpperCase and LowerCase

---
+++ src/TushCodes/StringUpAndLowCase.java
@@ -0,0 +1,30 @@
+package TushCodes;
+
+public class StringUpAndLowCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		//String UpperCase
+		
+		String FName = ("tushar");
+		
+		String fn = FName.toUpperCase();
+		
+		System.out.println(fn);
+		
+		
+		//String LowerCase
+		
+		String LName = ("INGALE");
+		
+		String ln = LName.toLowerCase();
+		
+		System.out.println(ln);
+		
+		
+	}
+
+}
