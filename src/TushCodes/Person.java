087a0b4135840f5254cc7ffc66463cb9a9f5cc6b
iyumtush <tusharcse0@gmail.com>
2026-07-16 13:24:29 +0530

    Changes in file

--- src/TushCodes/Person.java
+++ src/TushCodes/Person.java
@@ -2,4 +2,42 @@ package TushCodes;
 
 public class Person {
 
+	
+	
+	//Static variable
+	
+	private static int totalPerson = 0;
+	
+	//instances
+	private String name;
+	private int age ;
+	
+	
+	//Constructor
+	public  Person(String name ,int age ) {
+		
+		this.name = name ;
+		this.age = age;
+		totalPerson++;
+		
+		
+		
+
+	}
+	
+	//methods to display
+	public void showPersonName() {
+		
+		System.out.println("The name of the person is : " +name);
+	}
+	
+	public void showPersonAge() {
+		System.out.println("The age of the person is : "+age);
+	}
+	
+	public static void showTotalPerson() {
+		System.out.println("The total person present : "+totalPerson);
+	}
+
+	
 }
--- src/TushCodes/PersonInfo.java
+++ src/TushCodes/PersonInfo.java
@@ -5,6 +5,15 @@ public class PersonInfo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		Person p1 = new Person("Tushar" , 21);
+		Person p2 = new Person("Ved" , 21);
+		
+		p1.showPersonName();
+		p2.showPersonAge();
+		
+		Person.showTotalPerson();
+		
 	}
 
 }
