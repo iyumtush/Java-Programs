a80620694e28597f35f7123d3174dbb154f71cf9
iyumtush <tusharcse0@gmail.com>
2026-07-16 18:07:56 +0530

    Added file and Methods SavingsAccount

---
+++ src/ManoranjanBankOfIndia/SavingsAccount.java
@@ -0,0 +1,13 @@
+package ManoranjanBankOfIndia;
+
+public class SavingsAccount extends BankAccount {
+	
+	private double interest_rate;
+	
+	public SavingsAccount(int account_number , double balance , double interest_rate) {
+		
+		
+		
+	}
+
+}
