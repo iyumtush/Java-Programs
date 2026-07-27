3afd4398f06bfd7ec35b8105bc518fd552210078
iyumtush <tusharcse0@gmail.com>
2026-07-27 16:29:41 +0530

    Created Own LinkList

---
+++ src/DSAinJava/LinkedList002.java
@@ -0,0 +1,68 @@
+//Q1 Create an LinkedList Manually 
+
+package DSAinJava;
+
+class Node //This Class Represent the single node in our linkedList
+{
+	int data;
+	Node next;
+	Node(int data)
+	{
+		this.data = data;
+		this.next = null;
+	}
+}
+class LinkedList
+{
+	Node head;
+	void add(int data) 
+	{
+		Node newNode = new Node(data);	
+	
+	
+	if(head == null)
+	{
+		
+		head = newNode;
+	}else
+	{
+		Node current = head;
+		while(current.next!=null) 
+		{
+			current = current.next;
+		}
+		current.next = newNode;
+	}
+	}
+	
+	 void print()
+	{
+		Node current = head ;
+		while(current != null) 
+		{
+			System.out.println(current.data+" ");
+			current = current.next;
+		}
+	}
+}
+
+public class LinkedList002 
+{
+
+	public static void main(String[] args) 
+	{
+		LinkedList list = new LinkedList();
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		list.add(40);
+		list.add(50);
+		
+		list.print();
+
+
+
+		
+	}
+
+}
