2a49d4dbe9e573d490b2c99a150c79f34677e84a
iyumtush <tusharcse0@gmail.com>
2026-08-19 23:48:35 +0530

    Added Comment

--- DSAQues/JavaQuestions/RevString.java
+++ DSAQues/JavaQuestions/RevString.java
@@ -9,7 +9,7 @@ public class RevString
 		
 		for(int i = str.length() - 1 ; i >= 0; i--) 
 		{
-			reversedstr += str.charAt(i);
+			reversedstr += str.charAt(i); //It traverse and save each character reversed
 		}
 		System.out.println("The reversed string : "+reversedstr);
 	}
