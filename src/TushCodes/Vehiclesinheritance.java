d85cd5dae110bde020cb0f1e5882e79ae37d7caf
iyumtush <tusharcse0@gmail.com>
2026-08-19 23:44:56 +0530

    Added notes in inheritance file

--- src/TushCodes/Vehiclesinheritance.java
+++ src/TushCodes/Vehiclesinheritance.java
@@ -9,6 +9,7 @@ public class Vehiclesinheritance {
 		Car1 c  = new Car1();
 		Bike1 b = new Bike1();
 		
+		//Inheritance is a mechanism in which one object acquires all the properties and behaviors of a parent object. It is an important part of OOPs concept. Inheritance represents the IS-A relationship.
 		v.displayinfo();
 		v.fuelconsumption();
 		c.displayinfo();
