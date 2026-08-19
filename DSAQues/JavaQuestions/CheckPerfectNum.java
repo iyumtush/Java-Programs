ae0135fd09cdc237e0c7f4752f887d4edc8cd2a3
iyumtush <tusharcse0@gmail.com>
2026-08-20 00:01:36 +0530

    Completed Prog checking bug

--- DSAQues/JavaQuestions/CheckPerfectNum.java
+++ DSAQues/JavaQuestions/CheckPerfectNum.java
@@ -1,10 +1,40 @@
 package JavaQuestions;
 
-public class CheckPerfectNum {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class CheckPerfectNum 
+{
 
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number : ");
+		int num = sc.nextInt();
+		int sum = 0;
+		int perfectNum = 0;
+		for(int i = 1 ; i<= num ; i++)
+		{
+			if(num / i == 0 )
+			{
+				sum =+i;
+				sum = num;
+				break;
+			}
+			else
+			{
+				sum = num;
+			}
+		}
+		
+		if(sum == num)
+		{
+			System.out.println("Perfect");
+		}
+		else
+		{
+			System.out.println("Finshyt");
+		}
+		
 	}
 
 }
