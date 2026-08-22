ebb8c4141f486f8f9e12fe1f1830ab23f40eef85
iyumtush <tusharcse0@gmail.com>
2026-08-22 21:02:04 +0530

    Created new solution to find the GCD of two numbers

---
+++ DSAQues/JavaQuestions/FindGCDOfNumbers.java
@@ -0,0 +1,44 @@
+package JavaQuestions;
+
+import java.util.Scanner;
+
+public class FindGCDOfNumbers 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the first number : ");
+		int num1 = sc.nextInt();
+		
+		System.out.print("Enter the first number : ");
+		int num2 = sc.nextInt();
+		
+		int limit = 0;
+		int biggest = 0;
+		int GCD = 0 ;
+		
+		if(num1 > num2 )
+		{
+			limit = num1;
+		}
+		else
+		{
+			limit = num2;
+		}
+		for(int i = 1 ; i < limit ; i++)
+		{
+			if(num1 % i == 0 && num2 % i == 0)
+			{
+				if(i > biggest)
+				{
+					GCD = i;
+				}
+				
+			}
+		}
+		
+		System.out.print("\nThe GCD of "+num1+" & "+num2+" is : "+GCD);
+	}
+
+}
